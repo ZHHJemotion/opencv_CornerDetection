71043a39d236acc19daaade3b93e6981791c3aac
zhhjemotion <zhhjemotoin@hotmail.com>
2016-11-01 16:44:48 +0100

    Corner Detection --- 角点检测

---
+++ CornerDetection/CornerDetection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CornerDetection --- 角点检测
+//
+//  Created by ZHHJemotion on 2016/11/1.
+//  Copyright © 2016年 Lukas_Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    
+    
+    return 0;
+}
